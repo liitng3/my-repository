44ad25520fd0714ba6208a2a0b8f9b73f897fe00
liitng3 <898260697@qq.com>
2019-10-11 15:18:36 +0800

    add

---
+++ weixinmsg.js
@@ -0,0 +1,55 @@
+const formatMsg = require('./fmtwxmsg');
+
+function help() {
+    return `这是一个消息回复测试程序，会把消息原样返回，但是目前不支持视频类型的消息`;
+}
+//处理用户发过来的消息
+//第一个参数是解析后的用户消息
+//第二个参数是要返回的消息对象
+//基本处理过程根据用户发过来的消息判断消息类型并处理
+function userMsg(wxmsg, retmsg) {
+    /*
+        检测是否为文本消息，如果是文本消息则先要检测是不是支持的关键词回复。
+    */
+    if (wxmsg.MsgType == 'text') {
+        if (wxmsg.Content == 'help' || wxmsg.Content == '?' || wxmsg.Content == '？') {
+            retmsg.msg = help();
+            retmsg.msgtype = 'text';
+            return formatMsg(retmsg);
+        } else if (wxmsg.Content == 'hello' || wxmsg.Content == '你好'){
+
+            retmsg.msg = '你好，你可以输入一些关键字测试消息回复，输入help/?获取帮助';
+            retmsg.msgtype = 'text';
+            return formatMsg(retmsg);
+
+        }else if(wxmsg.Content == 'who' || wxmsg.Content == 'Who' || wxmsg.Content == 'WHO'){
+            retmsg.msg='开发者信息\n姓名：李婷\n性别：女\n学号：2017011811'
+            retmsg.msgtype = 'text';
+            return formatMsg(retmsg);
+        }else {
+            retmsg.msg = wxmsg.Content;
+            retmsg.msgtype = wxmsg.MsgType;
+            return formatMsg(retmsg);
+        }
+    } else {
+        switch(wxmsg.MsgType) {
+            case 'image':
+            case 'voice':
+                retmsg.msg = wxmsg.MediaId;
+                retmsg.msgtype = wxmsg.MsgType;
+                break;
+            default:
+                retmsg.msg = '不支持的类型';
+        }
+
+        return formatMsg(retmsg);
+    }
+}
+
+exports.userMsg = userMsg;
+exports.help = help;
+
+exports.msgDispatch = function msgDispatch(wxmsg, retmsg) {
+    return userMsg(wxmsg, retmsg);
+};
+
